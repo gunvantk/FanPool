3f2bcc614d106d5a6a833fb76fb859f1147e8f44
Sree <mavharsha@mavharsha.com>
2021-08-26 16:47:54 -0700

    Added deactivate method

--- src/components/wallet-button/WalletButton.tsx
+++ src/components/wallet-button/WalletButton.tsx
@@ -1,23 +1,18 @@
 import { useEthers } from "@usedapp/core";
 
 function WalletButton() {
-  const { activateBrowserWallet, account } = useEthers();
-
-  function handleConnectWallet() {
-    console.log('Yay!')
-    activateBrowserWallet();
-  }
+  const { activateBrowserWallet, account, deactivate } = useEthers();
 
     return(
       <div>
-          <button className="py-2 px-2 font-medium text-white bg-green-500 rounded hover:bg-green-400 transition duration-300" onClick={handleConnectWallet}>Connect Wallet</button>
-        {account && <p>Account: {account}</p>}
+        {!account && <button className="py-2 px-2 font-medium text-white bg-green-500 rounded hover:bg-green-400 transition duration-300" onClick={() => activateBrowserWallet()}>Connect Wallet</button>}
+        {account && 
+          <>
+            <button className="py-2 px-2 font-medium text-white bg-green-700 rounded hover:bg-green-600 transition duration-300" onClick={() => deactivate()}>Disconnect { `${account?.slice(0, 5)}...${account?.slice(-4)}`} </button>
+          </>
+        } 
       </div>
     );
 }
 
 export default WalletButton;
-
-/* <>
-<button className="py-2 px-2 font-medium text-white bg-green-500 rounded hover:bg-green-400 transition duration-300">Connect Wallet</button>
-</> */
